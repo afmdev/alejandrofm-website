1b8fc090deb4091116bf30711a5f878dd8698b45
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-01-08 14:52:33 +0100

    Blog page

---
+++ alejandrofm-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+function my_theme_enqueue_styles() { 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+add_filter( 'big_image_size_threshold', '__return_false' );
